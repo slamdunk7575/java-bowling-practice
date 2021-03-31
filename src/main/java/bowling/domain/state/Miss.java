dea1e02c4fb93fe4b6b15b768c0220d2ed0fbe4f
slamdunk7575 <slamdunk7575@gmail.com>
2021-03-31 22:23:57 +0900

    (feat) MISS 상태 추가

---
+++ src/main/java/bowling/domain/state/Miss.java
@@ -0,0 +1,46 @@
+package bowling.domain.state;
+
+import bowling.domain.frame.Score;
+import bowling.domain.pin.Pin;
+
+public class Miss extends FinishedState {
+
+    private final Pin first;
+    private final Pin second;
+
+    private Miss(Pin first, Pin second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static State of(Pin firstPin, Pin felledPin) {
+        return new Miss(firstPin, felledPin);
+    }
+
+    public Pin getFirstPin() {
+        return first;
+    }
+
+    public Pin getSecondPin() {
+        return second;
+    }
+
+    @Override
+    public Score calculate(Score baseScore) {
+        baseScore = baseScore.add(first.toScore());
+        if (baseScore.isPending()) {
+            baseScore = baseScore.add(second.toScore());
+        }
+        return baseScore;
+    }
+
+    @Override
+    public Score getScore() {
+        return first.add(second).toScore();
+    }
+
+    @Override
+    public boolean isFrameFinish(State state) {
+        return true;
+    }
+}

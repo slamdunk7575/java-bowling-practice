32d3bb9188cbd197af561c9f6a3a00823265dd70
slamdunk7575 <slamdunk7575@gmail.com>
2021-03-31 21:41:29 +0900

    (feat) Gutter 상태 추가

---
+++ src/main/java/bowling/domain/state/Gutter.java
@@ -0,0 +1,32 @@
+package bowling.domain.state;
+
+import bowling.domain.frame.Score;
+
+public class Gutter extends FinishedState {
+
+    private Gutter() {
+    }
+
+    public static State of() {
+        return new Gutter();
+    }
+
+    @Override
+    public Score calculate(Score baseScore) {
+        baseScore = baseScore.add(Score.ofGutter());
+        if(baseScore.isPending()) {
+            baseScore = baseScore.add(Score.ofGutter());
+        }
+        return baseScore;
+    }
+
+    @Override
+    public Score getScore() {
+        return Score.ofGutter();
+    }
+
+    @Override
+    public boolean isFrameFinish(State state) {
+        return true;
+    }
+}

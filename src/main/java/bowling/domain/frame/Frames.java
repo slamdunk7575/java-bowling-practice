97c2a604719839dba34418540bfe18da52dfb411
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-13 00:09:16 +0900

    (feat) Frames 일급 컬렉션 생성

---
+++ src/main/java/bowling/domain/frame/Frames.java
@@ -0,0 +1,38 @@
+package bowling.domain.frame;
+
+import bowling.domain.pin.Pin;
+
+import java.util.LinkedList;
+
+public class Frames {
+
+    private LinkedList<Frame> frames;
+
+    private Frames() {
+        this.frames = new LinkedList<>();
+        Frame firstFrame = Frame.init();
+        frames.add(firstFrame);
+    }
+
+    public static Frames init() {
+        return new Frames();
+    }
+
+    public void bowl(Pin felledPin) {
+        Frame currentFrame = frames.getLast().bowl(felledPin);
+        frames.add(currentFrame);
+    }
+
+    public boolean isGameOver() {
+        return frames.getLast().isEnd();
+    }
+
+    public Frame getFirstFrame() {
+        return frames.getFirst();
+    }
+
+    public Index getLastIndex() {
+        return frames.getLast().getIndex();
+    }
+
+}

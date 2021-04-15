ef7eb1e5f96445a51b3559efcfbe975aae368364
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-15 17:44:52 +0900

    (feat) InputView 추가

---
+++ src/main/java/bowling/view/InputView.java
@@ -0,0 +1,34 @@
+package bowling.view;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class InputView {
+    private static final String NAME_OF_PLAYER = "플레이어 {0}의 이름은(3 English Letters)?: ";
+    private static final String NUMBER_OF_PLAYER = "How many people? ";
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static List<String> getUserNames() {
+        System.out.println(NUMBER_OF_PLAYER);
+        return IntStream.rangeClosed(1, Integer.parseInt(scanner.nextLine()))
+                .mapToObj(InputView::getUserName)
+                .collect(toList());
+    }
+
+    public static String getUserName(int number) {
+        System.out.println(MessageFormat.format(NAME_OF_PLAYER, number));
+        return scanner.nextLine();
+    }
+
+    public static int getFelledPin() {
+        return scanner.nextInt();
+    }
+}
